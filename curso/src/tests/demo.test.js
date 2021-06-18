8e4036f428e2993a06924bbc29813b9da125095e
Gwynbleidd <u2guitarrista.vladimir@gmail.com>
2021-06-17 23:05:53 -0500

    Pruebas

--- curso/src/tests/demo.test.js
+++ curso/src/tests/demo.test.js
@@ -1,8 +1,20 @@
-test('Debe ser una prueba', () => {
-    const isActive = true;
+describe('Pruebas en el archivo demo.test.js', () => {
+    test('Deben ser una iguales los string', () => {
+        // const isActive = true;
 
-    if (isActive) {
-        throw new Error('No esta activo');
-    }
+        // if (isActive) {
+        //     throw new Error('No esta activo');
+        // }
+
+        //1. Inicialización
+        const mensaje = 'Hola mundo';
+
+        //2. estímulo
+        const mensaje2 = `Hola mundo`;
+
+        //3. Observar el comportamiento
+        expect(mensaje).toBe(mensaje2);
+    });
 });
+
 
