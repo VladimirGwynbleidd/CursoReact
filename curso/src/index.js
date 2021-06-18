3f0c936dbc289c59c475222ff137d8bdfac0a356
Gwynbleidd <u2guitarrista.vladimir@gmail.com>
2021-06-17 22:01:39 -0500

    Curso React

---
+++ curso/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import PrimeraApp from './PrimeraApp'
+import CounterApp from './CounterApp';
+
+import './index.css'
+
+
+// const saludo = <h1>Hola mundo</h1>
+const divRoot = document.querySelector('#app');
+
+ReactDOM.render(<CounterApp value={10} />, divRoot);
+
